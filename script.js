74e7e55bdbc3b98f47769e9c6d3d5582e160d2b2
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-04-30 23:14:24 +0100

    Only setInterval when reference has loaded

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ let timers = {
     lastStarWarsReference: 0
 }
 
-setInterval(() => {
-    let elapsed = (Date.now() - timers.lastStarWarsReference) / 1000;
-    let elapsedDays = Math.floor(elapsed / 86400);
-    let elapsedHours = Math.floor((elapsed % 86400) / 3600);
-    let elapsedMinutes = Math.floor((elapsed % 3600) / 60);
-    let elapsedSeconds = (elapsed % 60).toFixed(3);
-    timerSpan.innerHTML = `${elapsedDays} days, ${elapsedHours} hours, ${elapsedMinutes} minutes, ${elapsedSeconds} seconds.`;
-}, 200 * Math.random());
+setLastStarWarsReference(() => {
+    setInterval(() => {
+        let elapsed = (Date.now() - timers.lastStarWarsReference) / 1000;
+        let elapsedDays = Math.floor(elapsed / 86400);
+        let elapsedHours = Math.floor((elapsed % 86400) / 3600);
+        let elapsedMinutes = Math.floor((elapsed % 3600) / 60);
+        let elapsedSeconds = (elapsed % 60).toFixed(3);
+        timerSpan.innerHTML = `${elapsedDays} days, ${elapsedHours} hours, ${elapsedMinutes} minutes, ${elapsedSeconds} seconds.`;
+    }, 200 * Math.random());
+});
 
-setLastStarWarsReference();
-
-function setLastStarWarsReference () {
+function setLastStarWarsReference (callback) {
     let request = new XMLHttpRequest();
     request.open("GET", "https://json.extendsclass.com/bin/e3f8eb195b58", true);
     request.onreadystatechange = e => {
         if (request.readyState === 4) {
             timers = JSON.parse(request.responseText);
+            callback();
         }
     };
     request.send();
